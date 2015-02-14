1786cf2038ba858331c2d3d8bcd7d39f1942b282
Nick Picciuto <nick@spartansystems.co>
2015-02-14 07:41:01 -0600

    remove lists

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,7 +1,4 @@
 class Worker < ActiveRecord::Base
-
-  WORKER_DEGREE = %w(MBA-Currently \ Enrolled MBA-Graduated Bachelors Post \ Graduate Other)
-  WORKER_FOCUS = %w(Management Accounting Economics Finance Government \ Relations Marketing Entrepreneurship Operations IT Distribution Manufacturing Sales Health Care Leadership Research International Business Communications Other')
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
