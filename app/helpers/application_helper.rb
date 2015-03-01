a1c6881a3d001b7fb98a19d1cf035a8f69e1351c
Nick Picciuto <nick@spartansystems.co>
2015-03-01 14:37:58 -0500

    log in shuffle

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,28 @@ module ApplicationHelper
   def blog_img(image)
   	image.nil? ? 'blog-photo1.jpg' : image
   end
+
+  def project_link_path(type_user)
+    if either_user_signed_in?
+      contacts_path
+    else
+      send("new_#{type_user}_registration_path")
+    end
+  end
+
+  def either_user_signed_in?
+    worker_signed_in? || company_signed_in?
+  end
+
+  def which_user
+    worker_signed_in? ? 'worker' : 'company'
+  end
+
+  def preselected_option
+    if either_user_signed_in?
+      which_user == 'worker' ? 'MBA Applicant' : 'Post a project'
+    else
+      'please select a subject...'
+    end
+  end
 end
