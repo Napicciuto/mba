dc8f4515f95278607f375f2384b63906b3901deb
Nick Picciuto <nick@spartansystems.co>
2015-03-26 22:03:12 -0400

    fix devise users

--- db/migrate/20150327004658_change_field.rb
+++ db/migrate/20150327004658_change_field.rb
@@ -2,6 +2,5 @@ class ChangeField < ActiveRecord::Migration
   def change
   	remove_column :workers, :degree
   	remove_column :workers, :industry
-  	add_column :workers, :degree_id, :integer
   end
 end
